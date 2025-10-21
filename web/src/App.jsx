329ec066f49dc55527049e49a556cd88638feb60
Codency <apexp7472@gmail.com>
2025-10-21 12:55:59 -0700

    study

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Landing from "./pages/Landing";
 import Extension from "./pages/Extension";
